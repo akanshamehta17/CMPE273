8e2c1909c724860fcca949e7bd6444a1529ddcbb
akansha <akansha.mehta@sjsu.edu>
2017-02-16 17:39:41 -0800

    second commit

---
+++ socket-mon.py
@@ -0,0 +1,48 @@
+import psutil
+from collections import namedtuple
+import pandas 
+import re
+import os
+
+f = open('conn_details.csv', 'w')
+sconn = namedtuple('sconn', 'fd family type laddr raddr status pid')
+conn = psutil.net_connections(kind='tcp') 
+for n in conn:
+  b = [n.pid, n.laddr, n.raddr, n.status]
+  if b[2] != ():
+    print >>f, ("'%s','%s','%s','%s'" % (n.pid, '@'.join(map(str, n.laddr)), '@'.join(map(str,n.raddr)), n.status))
+
+f.close()
+
+cols = ['pid','laddr','raddr','status']
+data = pandas.read_csv('conn_details.csv', index_col=False, header=None, names=cols)
+g = data.groupby('pid').size().sort_values(ascending=False).reset_index(name='Count') #Creating dataframe g that has PID in order of number of connections it has based on column 'Count'
+
+f1 = open('out.txt', 'w')
+dg = data.groupby('pid') #returns a dictionary with 'pid' as key and connections as values
+for index, row in g.iterrows(): #iterating through g dataframe that has 'pid' and its 'Count'
+  k = row['pid']
+  print >>f1, dg.get_group(k) # returns groups based on key 'pid'
+f1.close()
+
+input_file = open('out.txt', 'r')
+print '\"pid\",\"laddr\",\"raddr\",\"status\"'
+for line in input_file:
+  matchobj = re.match(r"^\d+\s+\'(.*)\'\s+\'(.*)\'\s+\'(.*)\'\s+\'(.*)\'$",line)
+  if matchobj:
+    print ("\"%s\",\"%s\",\"%s\",\"%s\"" % (matchobj.group(1),matchobj.group(2),matchobj.group(3),matchobj.group(4)))
+
+
+os.remove("conn_details.csv")
+os.remove("out.txt")
+
+ 
+
+
+
+ 
+
+
+
+
+
